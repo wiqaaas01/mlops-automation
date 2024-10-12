a5075c0f489040423d1f9c0421a9bdcfd333b45a
wiqaaas01 <wiqaaas.01@gmail.com>
2024-10-12 04:58:16 +0000

    train error added

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ def train_model():
     data = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
     model = LogisticRegression(max_iter=200)
-    model.fit(X_train, y_train)
+    model.fitt(X_train, y_train)
     # added stuff over here e.g. ......	
     # .......
 
